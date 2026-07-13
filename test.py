2149bdc0fd88b9145569dd4041ece0cb995e3cdc
황성보 <ssbb7977@naver.com>
2026-07-13 09:58:06 +0900

    7월 13일

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
-T = int(input()) # 테스트 케이스 
-for _ in range(T):
-    N, M = map(int,input().split()) # N : 행렬 배열, M: 분사 범위 
-    max_value = 0 # 최대 파리 잡은 개수 
-    matrix = []
-    for _ in range(N):  # 행렬 자료구조 저장 
-        array = list(map(int,input().split()))
-        matrix.append(array)
-    
-    print("행렬:",matrix)
 
-    for idx, row in enumerate(matrix):
-        value = row[idx:idx+M] + row[idx:idx-M] + row[idx-M:idx]
+
+grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+row = len(grid)
+col = len(grid[0])
+# print(grid)
+# print(row)
+# print(col)
+count = 0   # 겹침 개수
+count_1 = 0 # 1의 개수  
+for i in range(row):
+    for j in range(col):
+        print(i)
+        print(j)
+        if grid[i][j] == 1:
+            count_1 += 1    
+        if grid[i][j] == 1 and grid[i][j-1] == 1:
+            count +=1 
+        if grid[i][j] == 1 and grid[i][j+1] == 1:
+            count +=1
+        if grid[i][j] == 1 and grid[i-1][j] == 1:
+            count +=1
+        if grid[i][j] == 1 and grid[i+1][j] == 1:
+            count +=1
+print(count_1*4)
+print(count)
+print(count_1*4 - count)
